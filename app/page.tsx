8760f836df87fd7dd198e6225d3e7876622253d1
Sultanov Abdurazzoq <abdurazzoqsultanov2208@gmail.com>
2024-08-07 17:49:09 +0500

    index page fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Header from '@/components/Header';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -9,17 +8,14 @@ export default function HomePage() {
     router.push('/dashboard')
   }
   return (
-    <>
-      <Header />
-      <div className="container mx-auto mt-8">
-        <h1 className="text-3xl font-bold mb-4">Welcome to FakeStore</h1>
-        <p className="mb-4">This is a sample e-commerce application using Next.js, Tailwind CSS, TypeScript, and shadcn/ui.</p>
-        <div className="flex space-x-4">
-          <Link href="/login">
-            <p className="bg-blue-500 text-white py-2 px-4 rounded-md">Login</p>
-          </Link>
-        </div>
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-4">Welcome to FakeStore</h1>
+      <p className="mb-4">This is a sample e-commerce application using Next.js, Tailwind CSS, TypeScript, and shadcn/ui.</p>
+      <div className="flex space-x-4">
+        <Link href="/login">
+          <p className="bg-blue-500 text-white py-2 px-4 rounded-md">Login</p>
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
